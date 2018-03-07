578c49a658306fd6a273395dc8c12fec9a42b9b8
otetsdiodor <otetsdiodor@gmail.com>
2018-03-07 12:55:52 +0200

    Add DBConnection Class

---
+++ TrackTimeManager/Services/DBConnectionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace TrackTimeManager.Services
+{
+    class DBConnectionService
+    {
+        static string ConnectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+        SqlConnection CurrentConnection;
+
+        private static SqlConnection Open()
+        {
+            return new SqlConnection();
+        }
+    }
+}
